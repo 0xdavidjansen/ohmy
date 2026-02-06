ec806d8dee13e5410fd37c22b23c015efba2003f
davidjansen <0xdavidjansen@pm.me>
2026-02-06 10:13:43 +0100

    Force light mode by default

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,8 @@ function AppContent() {
   const dismissibleWarnings = warnings.filter((w) => w.dismissible);
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 dark:from-slate-900 dark:to-slate-800">
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50">
+
 
       {/* Warning Banners */}
       {dismissibleWarnings.length > 0 && (
